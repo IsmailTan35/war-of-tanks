7ab419eef8b54f562c19939f2dc363ebf32c66a1
IsmailTan35 <tan_tan.2000@hotmail.com>
2023-06-21 21:43:55 +0300

    .

--- src/component/Weaponry/Connon/index.tsx
+++ src/component/Weaponry/Connon/index.tsx
@@ -1,56 +1,61 @@
-import { useCylinder, useSphere } from "@react-three/cannon";
-import { useFrame, useThree } from "@react-three/fiber";
-import React, { use, useEffect } from "react";
-import { MathUtils, Vector3 } from "three";
-
-const force = 200;
-
+import React, { useEffect } from "react";
+import { useSphere } from "@react-three/cannon";
+import { useThree } from "@react-three/fiber";
+import { MathUtils, Sphere, Vector3 } from "three";
 const Connon = (props: any) => {
   const { scene, gl, camera } = useThree();
-  const [isLaunched, setIsLaunched] = React.useState<boolean>(false);
   const {
-    disabled = false,
     layer,
     connonStep,
     degreX,
     degreY,
     args = [0.3],
-    position = [0, 2, -5],
+    position = [0, -4, 0],
   } = props;
-
   const [containerRef, api]: any = useSphere(() => ({
-    collisionResponse: false,
     mass: 1,
-    args,
+    collisionResponse: false,
+    args: [0.3],
     position,
   }));
 
   useEffect(() => {
     if (!containerRef.current) return;
+    const turret = scene.getObjectByName("turret");
 
-    const radyanX = MathUtils.degToRad(degreX);
-    const force = [0, 0, 100];
-    const tank = scene.getObjectByName("turret");
-    if (!tank) return;
+    if (!turret) return;
+
     const target = new Vector3();
-    tank?.getWorldPosition(target);
-    console.log(target);
+    containerRef.current?.getWorldPosition(target);
+    turret?.getWorldPosition(target);
 
-    api.position.set(target.x, target.y, target.z);
-    api.applyLocalImpulse(force, [1, 1, 0]);
+    const angleInRadians = (Math.PI / 180) * degreX;
+    const distanceFromCenter = 3;
+    const offsetX = distanceFromCenter * Math.cos(angleInRadians);
+    const offsetY = distanceFromCenter * Math.sin(angleInRadians);
+    console.log(turret.quaternion);
+    const newX = target.x + offsetX;
+    const newY = target.z + offsetY;
+    api.position.set(newX + 3, 1, newY);
+    const force = [0, 0, -500];
     scene.add(containerRef.current);
 
     setTimeout(() => {
       api.collisionResponse.set(true);
+      api.applyLocalImpulse(force, [0, 0, 0]);
     }, 100);
     setTimeout(() => {
-      scene.remove(containerRef.current);
+      // scene.remove(containerRef.current);
     }, 5000);
   }, [scene, containerRef, api]);
 
   return (
     <>
-      <mesh ref={containerRef} name="cannon">
+      <mesh position={[2, 2, 0]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshBasicMaterial color="black" />
+      </mesh>
+      <mesh ref={containerRef} name="cannon" position={position}>
         <sphereGeometry args={args} />
         <meshBasicMaterial color={"black"} />
         {/* <mesh position={[0, 0.3, 0]}>
