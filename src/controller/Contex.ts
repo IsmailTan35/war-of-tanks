c2d0e5c475be14fb56cae02b66faedad31973b1c
IsmailTan35 <tan_tan.2000@hotmail.com>
2023-06-24 22:56:52 +0300

    .

---
+++ src/controller/Contex.ts
@@ -0,0 +1,22 @@
+import React from "react";
+import { io } from "socket.io-client";
+
+export const url: any = () => {
+  const hostname: any = window.location.hostname;
+  const parsed: any = window.location.hostname.split(".");
+  const protocol: any = window.location.protocol;
+
+  return parsed.includes("vercel")
+    ? process.env.REACT_APP_URL_PRODUCTION
+    : `${protocol}//${hostname}:11000`;
+};
+export const client = io(
+  url().replace("http://", "").replace("https://", "").replace("/", ""),
+  {
+    reconnection: true,
+    reconnectionDelay: 2500,
+    reconnectionAttempts: 10,
+    forceNew: true,
+  }
+);
+export const SocketContext: any = React.createContext(null);
